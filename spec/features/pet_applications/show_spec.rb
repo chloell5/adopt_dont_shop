33b7421d1c42ad616fd2402199456997180c199c
ChloeLL <chloell@protonmail.ch>
2021-10-26 22:46:31 -0700

    Add functionality for partial and case issues in application pet search

--- spec/features/pet_applications/show_spec.rb
+++ spec/features/pet_applications/show_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe 'pet application show' do
   context 'basic information' do
     it 'shows the applicant details' do
-      app = PetApplication.create!(name: 'Liam', street: '155 Main Street', city: 'Phoenix', state: 'AZ', zip: 85_222,
-                                   reason: 'Pet friendly, loving home looking for a companion')
+      app = PetApplication.create!(name: 'Liam', street: '155 Main Street', city: 'Phoenix', state: 'AZ', zip: 85_222)
 
       visit "/applications/#{app.id}"
 
@@ -13,12 +12,10 @@ RSpec.describe 'pet application show' do
       expect(page).to have_content(app.city)
       expect(page).to have_content(app.state)
       expect(page).to have_content(app.zip)
-      expect(page).to have_content(app.reason)
     end
 
     it 'shows application status' do
-      app = PetApplication.create!(name: 'Liam', street: '155 Main Street', city: 'Phoenix', state: 'AZ', zip: 85_222,
-                                   reason: 'Pet friendly, loving home looking for a companion')
+      app = PetApplication.create!(name: 'Liam', street: '155 Main Street', city: 'Phoenix', state: 'AZ', zip: 85_222)
 
       visit "/applications/#{app.id}"
 
@@ -53,40 +50,69 @@ RSpec.describe 'pet application show' do
     end
   end
 
-  context 'adding pets to application' do
+  context 'search bar' do
     before(:each) do
-      @app = PetApplication.create!(name: 'Liam', street: '155 Main Street', city: 'Phoenix', state: 'AZ', zip: 85_222,
-                                    reason: 'Pet friendly, loving home looking for a companion')
+      @app = PetApplication.create!(name: 'Liam', street: '155 Main Street', city: 'Phoenix', state: 'AZ', zip: 85_222)
     end
-    it 'has a search bar if app is not submitted' do
+    it 'has search functions if app is not submitted' do
       visit "/applications/#{@app.id}"
 
-      within('main') {expect(find('form')).to have_content('Search')}
+      within('main') { expect(find('form')).to have_content('Search') }
     end
 
-    xit 'has no search function if app is submitted' do
-      #not sure this will work until I move the form to a partial
+    xit 'has no search functions if app is submitted' do
+      # not sure this will work until I move the form to a partial
       @app.status = 1
       visit "/applications/#{@app.id}"
 
       expect(page).to_not have_content('Add a Pet to this Application')
     end
+  end
 
-# ADD TESTS FOR PARTIAL MATCHES
-    it 'searches for pet by name' do
-      shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
-      pet = Pet.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: shelter.id)
+  context 'search functionality' do
+    before(:each) do
+      @app = PetApplication.create!(name: 'Liam', street: '155 Main Street', city: 'Phoenix', state: 'AZ', zip: 85_222)
+      @shelter = Shelter.create(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+      @pet = Pet.create(name: 'Scrappy', age: 1, breed: 'Great Dane', adoptable: true, shelter_id: @shelter.id)
+    end
 
+    it 'searches for pet by exact name' do
       visit "/applications/#{@app.id}"
 
       fill_in 'Pet Name Search', with: 'Scrappy'
       click_on 'Search Pets'
 
-      expect(page).to have_content(pet.name)
-      expect(page).to have_content(pet.age)
-      expect(page).to have_content(pet.breed)
-      expect(page).to have_content(pet.adoptable)
-      expect(page).to have_content(pet.shelter.name)
+      expect(page).to have_content(@pet.name)
+      expect(page).to have_content(@pet.age)
+      expect(page).to have_content(@pet.breed)
+      expect(page).to have_content(@pet.adoptable)
+      expect(page).to have_content(@pet.shelter.name)
+    end
+
+    it 'searches for pet by partial match' do
+      visit "/applications/#{@app.id}"
+
+      fill_in 'Pet Name Search', with: 'Scrap'
+      click_on 'Search Pets'
+
+      expect(page).to have_content(@pet.name)
+      expect(page).to have_content(@pet.age)
+      expect(page).to have_content(@pet.breed)
+      expect(page).to have_content(@pet.adoptable)
+      expect(page).to have_content(@pet.shelter.name)
+    end
+
+    it 'searches for pet without case sensitivity' do
+      visit "/applications/#{@app.id}"
+
+      fill_in 'Pet Name Search', with: 'scRaPpy'
+      click_on 'Search Pets'
+
+      expect(page).to have_content(@pet.name)
+      expect(page).to have_content(@pet.age)
+      expect(page).to have_content(@pet.breed)
+      expect(page).to have_content(@pet.adoptable)
+      expect(page).to have_content(@pet.shelter.name)
     end
   end
 end
